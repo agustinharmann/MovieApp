fb1a5be525838b549c129cfec8f926a5140897b6
agustinharmann <agustinharmann@gmail.com>
2023-03-14 01:27:59 -0300

    change styles css at styles of boostrap in the home component

--- src/components/GenresCards/index.jsx
+++ src/components/GenresCards/index.jsx
@@ -3,10 +3,6 @@ import './styles.css';
 
 const GenresCards = ({ genre }) => {
   const {id} = genre;
-  // console.log(genre);
-  // console.log(id);
-  // console.log(genres);
-  // console.log(genres.id);
   return (
     <Link to={'/genre'} state={{ some: id }}>
       <div className='genres--cards'>
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -3,7 +3,6 @@ import { UserContext } from '../../utils/userProvider';
 import { Link } from 'react-router-dom';
 import { MapMovies } from '../MapMovies';
 import { MapGenres } from '../MapGenres';
-import './styles.css';
 import { AiOutlineSearch } from 'react-icons/ai';
 
 const Home = () => {
@@ -16,37 +15,36 @@ const Home = () => {
 
   return (
     <div className='home'>
-      <div className='d-flex justify-content-between w-100 my-3'>
-        <div className='app_titile--header'>MoviesApp</div>
-        <div className='search--home d-flex align-items-center'>
-          <Link to={'/movies-searched'}>
-            <button className='btn_search-movie--home btn btn-primary'>Search movie <AiOutlineSearch className='mx-1' /></button>
+      <div className="d-flex justify-content-between flex-wrap w-100 my-3">
+        <div className="fs-1 fw-bold text-primary">MoviesApp</div>
+        <div className="d-flex align-items-center">
+          <Link to={"/movies-searched"}>
+            <button className="btn btn-primary">
+              <AiOutlineSearch className="mx-1" />
+              Search movie
+            </button>
           </Link>
         </div>
       </div>
-      <div className="d-flex justify-content-between align-items-center w-100 mt-1">
-        <div className='title_trendings--home'>Trendings of day</div>
-        <button className='btn_trending--home btn btn-primary'>
-          <Link className='text_btn-trending--home' to={'/Trending'}>
-            View all
-          </Link>
-        </button>
+      <div className="d-flex justify-content-between align-items-center flex-wrap w-100 mt-1">
+        <div className="fs-5 fw-bold text-primary">Trendings of day</div>
+        <a href="/Trending" className="btn btn-primary">
+          View all
+        </a>
       </div>
-      <div className="trending--home">
-        <MapMovies
-          mapeable={movies}
-        />
+      <div className="d-flex overflow-auto">
+        <MapMovies mapeable={movies} />
       </div>
-      <div className='categories--home'>
-        <div className='categories_title--home'>
-          Categories
+        <div className='my-5'>
+          <div className='mb-1 fs-4 fw-bold text-primary'>
+            Categories
+          </div>
+          <div className='overflow-auto'>
+            <MapGenres
+              mapeable={genres}
+            />
+          </div>
         </div>
-        <div className='genres--home'>
-          <MapGenres
-            mapeable={genres}
-          />
-        </div>
-      </div>
     </div>
   );
 };
