f7beb7e1b3487369d92a9bdd7250345c6a796a68
agustinharmann <agustinharmann@gmail.com>
2023-03-15 04:12:49 -0300

    chenge styles at boostrap styles in MovieCard component

--- src/components/MovieCard/index.jsx
+++ src/components/MovieCard/index.jsx
@@ -2,14 +2,11 @@ import { Link } from 'react-router-dom';
 import './styles.css';
 
 const MovieCard = ({ movieCard }) => {
-  // console.log(movieCard);
   const {id} = movieCard;
   return (
-    // entrara verlo desde la peli de avatar que la peli de airon ma  no tienen poster path
     <Link to={'/info-movie'} state={{ some: id }}>
-      <div className='movie--card'>
-        { movieCard.poster_path ? <img className='movie_img--card' src={`https://image.tmdb.org/t/p/w200${movieCard.poster_path}`} alt={movieCard.title} /> : movieCard.title}
-        {/* agregar validacion del poster path no disponible */}
+      <div className='movie--card my-3 mx-1'>
+        { movieCard.poster_path ? <img className='movie_img--card rounded-3' src={`https://image.tmdb.org/t/p/w200${movieCard.poster_path}`} alt={movieCard.title} /> : movieCard.title}
       </div>
     </Link>
   );
