d24180aa0904e3d5e2585bdc0ab22ef7fe55d485
agustinharmann <agustinharmann@gmail.com>
2023-04-14 15:56:15 -0300

    order the links of footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import { BsGithub, BsLinkedin  } from 'react-icons/bs';
+import { BsCodeSlash, BsGithub, BsLinkedin } from 'react-icons/bs';
 import './styles.css';
 
 const Footer = () => {
   return (
     <footer className='footer px-3'>
       <section className='container-section--footer d-flex'>
-        <div className='column--footer d-flex'>
+        <div className='column--footer d-flex flex-column'>
+          <a className='info--footer d-flex align-items-center my-2' href='https://github.com/agustinharmann/MovieApp' target='_blank' rel='noreferrer'>
+            <BsCodeSlash className='icon--footer' />
+            View the code
+          </a>
           <a className='info--footer d-flex  my-2' href='https://www.themoviedb.org/documentation/api' target='_blank' rel='noreferrer'>
             Documentation of API
           </a>
         </div>
         <div className='column--footer d-flex flex-column'>
-          <a className='info--footer d-flex align-items-center my-2' href='https://github.com/agustinharmann' target='_blank' rel='noreferrer'>
-            <BsGithub className='icon--footer' />
-            Github
-          </a>
           <a className='info--footer d-flex align-items-center my-2' href='https://www.linkedin.com/in/agustin-harmann-1a4794269/' target='_blank' rel='noreferrer'>
             <BsLinkedin className='icon--footer' />
             Linkedin
+          </a>
+          <a className='info--footer d-flex align-items-center my-2' href='https://github.com/agustinharmann' target='_blank' rel='noreferrer'>
+            <BsGithub className='icon--footer' />
+            Github
           </a>
         </div>
       </section>
